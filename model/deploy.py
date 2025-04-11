bce6be809908777b9c760e53d77f665a5aee89ae
Deep Goyal <deep.goyal@icloud.com>
2025-04-10 21:33:23 -0700

    deploy bentune on aws

--- model/deploy.py
+++ model/deploy.py
@@ -18,24 +18,31 @@ load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '..', '.env'))
 region = os.getenv('AWS_REGION')
 boto_session = boto3.Session(region_name=region)
 sagemaker_session = sagemaker.Session(boto_session=boto_session)
+role = os.getenv("AWS_ROLE")
+endpoint_name = os.getenv("AWS_ENDPOINT_NAME")
 
-# dummy model
-hub = {
-    'HF_MODEL_ID': 'distilgpt2',
-    'HF_TASK': 'text-generation'
-}
+#paths
+model_dir = os.path.join(os.path.dirname(__file__), 'llama-3.2-3b-4bit.tar.gz')
+entry_point = os.path.join(os.path.dirname(__file__), 'handler.py')
+s3_model_path = sagemaker_session.upload_data(path=model_dir, key_prefix="llama-3.2-3b-model")
 
-huggingface_model = HuggingFaceModel(
-    env=hub,
-    role=os.getenv("AWS_ROLE"),
+# make a bucket
+hf_model = HuggingFaceModel(
+    model_data=s3_model_path,
+    role=role,
     transformers_version="4.26",
     pytorch_version="1.13",
     py_version="py39",
-    sagemaker_session=sagemaker_session,
+    entry_point=entry_point,
+    source_dir=os.path.dirname(__file__),
+    sagemaker_session=sagemaker_session
 )
 
-predictor = huggingface_model.deploy(
+#deploy
+predictor = hf_model.deploy(
     initial_instance_count=1,
-    instance_type="ml.t2.medium",
-    endpoint_name=os.getenv("SAGEMAKER_ENDPOINT_NAME"),
+    instance_type="ml.g4dn.xlarge",
+    endpoint_name=endpoint_name
 )
+
+print(f"Model deployed! -- success at : {predictor.endpoint_name}")
