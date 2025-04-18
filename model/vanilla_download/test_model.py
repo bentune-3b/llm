ce2bb2ff454316136ec7c352783cd2b825326796
jay2044 <117245060+jay2044@users.noreply.github.com>
2025-04-18 01:00:50 -0700

    model test

--- model/vanilla_download/test_model.py
+++ model/vanilla_download/test_model.py
@@ -13,4 +13,4 @@ while True:
     inputs = tokenizer(prompt, return_tensors="pt").to("cuda")
     outputs = model.generate(**inputs, max_new_tokens=300)
     response = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    print("\n\nModel:", response)
+    print("Model:", response)
