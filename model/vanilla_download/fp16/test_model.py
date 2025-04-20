4afa36bd469d84ea3c8a3b9fb063e15e4165b6f5
jay2044 <117245060+jay2044@users.noreply.github.com>
2025-04-20 01:28:19 -0700

    ok paths should be fixed

--- model/vanilla_download/fp16/test_model.py
+++ model/vanilla_download/fp16/test_model.py
@@ -1,7 +1,8 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
+import os
 
-model_path = "~/bentune/model/downloaded_models/vanilla-llama-3.2-3b-fp16"
+model_path = os.path.expanduser("~/bentune/model/downloaded_models/vanilla-llama-3.2-3b-fp16")
 
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 model = AutoModelForCausalLM.from_pretrained(model_path, torch_dtype=torch.float16).cuda()
